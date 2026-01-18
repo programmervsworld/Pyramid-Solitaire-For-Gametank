7122b6105a5bfe1a0d3c0f4c657b53c0c6c8b01a
jdowning <programmervsworld@gmail.com>
2026-01-18 01:11:52 +0000

    Have it dealing the pyramic offset

---
+++ src/cards.h
@@ -0,0 +1,35 @@
+#ifndef CARDS_H
+#define CARDS_H
+
+#include "gt/feature/random/random.h"
+
+int values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+int divx[] = {16, 32, 48, 64, 80, 96, 112};
+int divy[] = {16, 32, 48, 64, 80, 96, 112};
+
+
+void loadDeck(int deck[])
+{
+    int i = 0;
+    for (i = 1; i < 52; i++)
+    {
+        deck[i - 1] = i;
+    }
+}
+
+void shuffleDeck(int cards[])
+{
+    int valueToSwap;
+    int indexToSwap;
+    int i;
+
+    for (i = 0; i < 51; i++)
+    {
+        indexToSwap = rnd_range(1, 50);
+        valueToSwap = cards[indexToSwap];
+        cards[indexToSwap] = cards[1];
+        cards[1] = valueToSwap;
+    }
+}
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,52 +1,98 @@
 #include "gt/gametank.h"
 #include "gt/gfx/draw_queue.h"
 #include "../gen/assets/cardframes.h"
+#include "cards.h"
+
+#define PIXEL_WIDTH 128
+#define PIXEL_HEIGHT 128
+#define CARD_HEIGHT 16
+#define CARD_WIDTH 16
 
 char box_x = 30, box_y = 20;
 char dx = 1, dy = 1;
-
+char rows = PIXEL_WIDTH / CARD_WIDTH;
+char cols = PIXEL_HEIGHT / CARD_HEIGHT;
+int deck[52];
 SpriteSlot testSlot;
+SpriteSlot textSlot;
 int frame = 1;
 
-void incrementFrame(){
-    if(++frame == 52){
+void setup()
+{
+    int i = 0;
+    testSlot = allocate_sprite(&ASSET__cardframes__frame_deck_bmp_load_list);
+    set_sprite_frametable(testSlot, &ASSET__cardframes__frame_deck_json);
+    loadDeck(deck);
+    shuffleDeck(deck);
+}
+
+void incrementFrame()
+{
+    if (++frame == 52)
+    {
         frame = 0;
     }
 }
 
+void main()
+{
 
-void main () {
-    testSlot = allocate_sprite(&ASSET__cardframes__frame_deck_bmp_load_list);
-    set_sprite_frametable(testSlot, &ASSET__cardframes__frame_deck_json);
+    setup();
 
-    while (1) {                                     //  Run forever
+    while (1)
+    { //  Run forever
+        int rows, cols, cardx, cardy, num, counter;
+
+        num = 0;
+        counter = 1;
+
         queue_clear_screen(3);
-        //queue_draw_box(box_x, box_y, 8, 8, 92);
-        //queue_draw_sprite(box_x, box_y, 16, 16, 16, 0, testSlot);
-        queue_draw_sprite_frame(testSlot, box_x, box_y, frame, false);
         queue_clear_border(0);
-        
-        box_x += dx;
+
+        for (rows = 0; rows < 7; rows++)
+        {
+            cardy = divy[rows];
+
+            if(rows != 0){
+                cardy -= rows * 4;
+            }
+           
+            for (cols = 0; cols < counter; cols++)
+            {
+                cardx = divx[cols];
+                queue_draw_sprite_frame(testSlot, cardx, cardy, deck[num], false);
+                num++;
+            }
+            counter++;
+        }
+
+        // queue_draw_sprite_frame(testSlot, box_x, box_y, deck[frame], false);
+
+        /*box_x += dx;
         box_y += dy;
-        if(box_x == 1) {
+        if (box_x == 1)
+        {
             dx = 1;
             incrementFrame();
-        } else if(box_x == 119) {
+        }
+        else if (box_x == 119)
+        {
             dx = -1;
             incrementFrame();
         }
-        if(box_y == 8) {
+        if (box_y == 8)
+        {
             dy = 1;
             incrementFrame();
-        } else if(box_y == 112) {
+        }
+        else if (box_y == 112)
+        {
             dy = -1;
             incrementFrame();
-        }
- 
+        }*/
+
         await_draw_queue();
-        await_vsync(1
-        );
+        await_vsync(1);
         flip_pages();
- 
     }
 }
