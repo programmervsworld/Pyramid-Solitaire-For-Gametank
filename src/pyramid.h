dbaee0478c6adfdc0facb19ac6f1c7eddc163534
jdowning <programmervsworld@gmail.com>
2026-01-23 02:30:03 +0000

    Finally have eligibility selection working

--- src/pyramid.h
+++ src/pyramid.h
@@ -11,8 +11,14 @@
 #define FLIPPED_PILE_X 80
 #define FLIPPED_PILE_Y 112
 #define DISCARD_BEGINS 27
-#define RANDOM_SEED 7
+#define RANDOM_SEED 9
 #define CHECK_BIT(var, pos) ((var >> pos) & 1)
+
+typedef struct
+{
+  int row;
+  int card;
+} CardPosition;
 
 // Used as a value table for figuring out what cards total to 13
 int values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
@@ -24,12 +30,15 @@ int divy[] = {16, 32, 48, 64, 80, 96, 112};
 // This is the playfield
 int board[7][7];
 int select[7][7];
+CardPosition eligibileCards[7];
 
 // The is the deck that we deal from and where the next discard will come from
 int deck[52];
 int discardPtr = 0;
 int cursorRow = 6;
 int cursorCard = 0;
+int eligPos = 0;
+int eligCount = 7;
 int numSelections = 0;
 int deckflips = 0;
 
@@ -45,11 +54,51 @@ char *selectionText = "Selected Card 1: ";
 
 void breakpoint() {}
 
-int findSelectableRowAboveMe(int cardPos) {
+void scanForEligibility()
+{
+  int row, col, entry;
+  for (entry = 0; entry < 6; entry++)
+  {
+    CardPosition pos;
+    eligibileCards[entry] = pos;
+  }
+  entry = 0;
+  for (col = 0; col < 7; col++)
+  {
+    for (row = 6; row > 0; row--)
+    {
+      if (row == 6 && board[row][col] > 0)
+      {
+        CardPosition pos;
+        pos.row = row;
+        pos.card = col;
+
+        eligibileCards[entry] = pos;
+        entry++;
+      }
+      else if (board[row][col] > 0 && board[row + 1][col] == 0 && board[row + 1][col + 1] == 0)
+      {
+        CardPosition pos;
+        pos.row = row;
+        pos.card = col;
+
+        eligibileCards[entry] = pos;
+        entry++;
+      }
+    }
+  }
+  eligCount = entry;
+}
+
+int findSelectableRowAboveMe(int cardPos)
+{
   int i;
-  for (i = 6; i >= 0; i--) {
-    if (board[i][cardPos] > 0) {
-      if (i < 6 && board[i + 1][cardPos + 1] == 0) {
+  for (i = 6; i >= 0; i--)
+  {
+    if (board[i][cardPos] > 0)
+    {
+      if (i < 6 && board[i + 1][cardPos + 1] == 0)
+      {
         return i;
       }
     }
@@ -57,24 +106,39 @@ int findSelectableRowAboveMe(int cardPos) {
   return -1;
 }
 
-void moveRightRedux() {
+void moveRightRedux()
+{
   bool searching = true;
   int nextSelectableRow;
-  if (isOnBoard) {
-    while (searching) {
-      if (cursorCard++ > 5) {
+  if (isOnBoard)
+  {
+    while (searching)
+    {
+      if (cursorCard++ > 5)
+      {
         cursorCard = 0;
       }
-      if (board[cursorRow][cursorCard] == 0 && cursorCard < 6) {
+      else
+      {
+        eligPos++;
+      }
+      if (board[cursorRow][cursorCard] == 0 && cursorCard < 6)
+      {
         nextSelectableRow = findSelectableRowAboveMe(cursorCard);
-        if (nextSelectableRow > -1) {
+        if (nextSelectableRow > -1)
+        {
           cursorRow = nextSelectableRow;
           searching = false;
         }
-      } else {
-        if (cursorRow < 6 && board[cursorRow + 1][cursorCard + 1] > 0) {
+      }
+      else
+      {
+        if (cursorRow < 6 && board[cursorRow + 1][cursorCard + 1] > 0)
+        {
           cursorRow++;
-        } else {
+        }
+        else
+        {
           searching = false;
         }
       }
@@ -82,25 +146,67 @@ void moveRightRedux() {
   }
 }
 
-void moveLeftRedux() {
+void moveRightUsingEligibility()
+{
+  CardPosition pos;
+
+  if (eligPos < eligCount)
+  {
+    eligPos++;
+  }
+
+  pos = eligibileCards[eligPos];
+  if (eligPos == 0 || (pos.row > 0 && pos.card > 0))
+  {
+    cursorRow = pos.row;
+    cursorCard = pos.card;
+  }
+}
+
+void moveLeftUsingEligibility()
+{
+  CardPosition pos;
+
+  if (eligPos > 0)
+  {
+    eligPos--;
+  }
+
+  pos = eligibileCards[eligPos];
+  cursorRow = pos.row;
+  cursorCard = pos.card;
+}
+
+void moveLeftRedux()
+{
   bool searching = true;
   int nextSelectableRow;
-  if (isOnBoard) {
-    while (searching) {
-      if (cursorCard-- <= 0) {
+  if (isOnBoard)
+  {
+    while (searching)
+    {
+      if (cursorCard-- <= 0)
+      {
         cursorCard = 6;
       }
-      if (board[cursorRow][cursorCard] == 0 && cursorCard < 6) {
+      if (board[cursorRow][cursorCard] == 0 && cursorCard < 6)
+      {
         nextSelectableRow = findSelectableRowAboveMe(cursorCard);
-        if (nextSelectableRow > -1) {
+        if (nextSelectableRow > -1)
+        {
           cursorRow = nextSelectableRow;
           searching = false;
         }
-      } else {
-        if (cursorRow < 6 && board[cursorRow + 1][cursorCard] > 0) {
+      }
+      else
+      {
+        if (cursorRow < 6 && board[cursorRow + 1][cursorCard] > 0)
+        {
           cursorRow++;
           searching = false;
-        } else {
+        }
+        else
+        {
           searching = false;
         }
       }
@@ -108,47 +214,59 @@ void moveLeftRedux() {
   }
 }
 
-void moveBackDiscard() {
+void moveBackDiscard()
+{
   bool searching = true;
 
-  while (searching) {
-    if (discardPtr == DISCARD_BEGINS) {
+  while (searching)
+  {
+    if (discardPtr == DISCARD_BEGINS)
+    {
       searching = false;
       break;
     }
 
-    if (deck[discardPtr - 1] > 0) {
+    if (deck[discardPtr - 1] > 0)
+    {
       searching = false;
     }
     discardPtr--;
   }
 }
 
-void advanceNextDiscard() {
+void advanceNextDiscard()
+{
   bool searching = true;
 
-  while (searching) {
-    if (deckflips == 3) {
+  while (searching)
+  {
+    if (deckflips == 3)
+    {
       searching = false;
       break;
       // Game over
     }
-    if (discardPtr + 1 == 51) {
+    if (discardPtr + 1 == 51)
+    {
       discardPtr = DISCARD_BEGINS;
       deckflips++;
     }
-    if (deck[discardPtr + 1] > 0) {
+    if (deck[discardPtr + 1] > 0)
+    {
       searching = false;
     }
     discardPtr++;
   }
 }
 
-void iterateBoard(void (*execute)(int row, int col, int search), int search) {
+void iterateBoard(void (*execute)(int row, int col, int search), int search)
+{
   int rows, cols;
 
-  for (rows = 0; rows < 7; rows++) {
-    for (cols = 0; cols < 7; cols++) {
+  for (rows = 0; rows < 7; rows++)
+  {
+    for (cols = 0; cols < 7; cols++)
+    {
       execute(rows, cols, search);
     }
   }
@@ -156,40 +274,52 @@ void iterateBoard(void (*execute)(int row, int col, int search), int search) {
 
 void zeroRecord(int row, int col, int search) { select[row][col] = 0; }
 
-void removeCard(int row, int col, int search) {
-  if (board[row][col] == search) {
+void removeCard(int row, int col, int search)
+{
+  if (board[row][col] == search)
+  {
     board[row][col] = 0;
   }
 }
 
-void canSelect(int row, int col, int search) {
-  if (row == 7 && board[row][col] > 0) {
+void canSelect(int row, int col, int search)
+{
+  if (row == 7 && board[row][col] > 0)
+  {
     select[row][col] |= 2;
-  } else {
+  }
+  else
+  {
     select[row][col] |= 2;
   }
 }
 
-void resetSelections() {
+void resetSelections()
+{
   iterateBoard(zeroRecord, 0);
   numSelections = 0;
   flipIsSelected = false;
 }
 
-void removeCardFromBoard(int cardNo) {
-  if (cardNo > 0) {
+void removeCardFromBoard(int cardNo)
+{
+  if (cardNo > 0)
+  {
     iterateBoard(removeCard, cardNo);
   }
 }
 
 void determineSelectability() { iterateBoard(canSelect, 0); }
 
-void loadPyramidBoard() {
+void loadPyramidBoard()
+{
   int r, c, num, counter;
   num = 0;
   counter = 1;
-  for (r = 0; r < 7; r++) {
-    for (c = 0; c < counter; c++) {
+  for (r = 0; r < 7; r++)
+  {
+    for (c = 0; c < counter; c++)
+    {
       board[r][c] = deck[num];
       num++;
     }
@@ -198,20 +328,25 @@ void loadPyramidBoard() {
   discardPtr = DISCARD_BEGINS;
 }
 
-void loadPyramidDeck(int *deck[]) {
+void loadPyramidDeck(int *deck[])
+{
   int i = 0;
-  for (i = 1; i < 52; i++) {
+  for (i = 1; i < 52; i++)
+  {
     deck[i - 1] = i;
   }
 }
 
-void shufflePyramidDeck(int *cards[], int times) {
+void shufflePyramidDeck(int *cards[], int times)
+{
   int valueToSwap;
   int indexToSwap;
   int i, t;
 
-  for (t = 0; t < times; t++) {
-    for (i = 0; i < 51; i++) {
+  for (t = 0; t < times; t++)
+  {
+    for (i = 0; i < 51; i++)
+    {
       indexToSwap = rnd_range(1, 50);
       valueToSwap = cards[indexToSwap];
       cards[indexToSwap] = cards[1];
@@ -220,7 +355,8 @@ void shufflePyramidDeck(int *cards[], int times) {
   }
 }
 
-void draw_selection() {
+void draw_selection()
+{
   text_init();
   text_cursor_x = 1;
   text_cursor_y = 7;
@@ -228,15 +364,18 @@ void draw_selection() {
   text_print_string(selectionText);
 }
 
-int getvalue(int cardno) {
+int getvalue(int cardno)
+{
   int value = cardno % 13;
-  if (value == 0) {
+  if (value == 0)
+  {
     value = 13;
   }
   return value;
 }
 
-void initializePyramidScene() {
+void initializePyramidScene()
+{
   background = allocate_sprite(&ASSET__backgrounds__background_bmp_load_list);
   testSlot = allocate_sprite(&ASSET__cardframes__frame_deck_bmp_load_list);
   set_sprite_frametable(testSlot, &ASSET__cardframes__frame_deck_json);
@@ -246,6 +385,7 @@ void initializePyramidScene() {
   loadPyramidBoard();
   resetSelections();
   determineSelectability();
+  scanForEligibility();
 
   text_init();
   text_load_font();
@@ -254,20 +394,27 @@ void initializePyramidScene() {
   text_print_width = 128;
 }
 
-void checkSelection() {
+void checkSelection()
+{
   int cardOne, cardTwo, rows, cols, value1, value2, total;
   cardOne = 0;
   cardTwo = 0;
   value1 = 0;
   value2 = 0;
   total = 0;
-  for (rows = 0; rows < 7; rows++) {
-    for (cols = 0; cols < 7; cols++) {
-      if (select[rows][cols] == 1) {
-        if (cardOne == 0) {
+  for (rows = 0; rows < 7; rows++)
+  {
+    for (cols = 0; cols < 7; cols++)
+    {
+      if (select[rows][cols] == 1)
+      {
+        if (cardOne == 0)
+        {
           cardOne = board[rows][cols];
           value1 = getvalue(cardOne + 1);
-        } else {
+        }
+        else
+        {
           cardTwo = board[rows][cols];
           value2 = getvalue(cardTwo + 1);
         }
@@ -277,7 +424,8 @@ void checkSelection() {
   }
 
   total = value1 + value2;
-  if (flipIsSelected) {
+  if (flipIsSelected)
+  {
     total = value1 + getvalue(deck[discardPtr] + 1);
   }
 
@@ -289,62 +437,91 @@ void checkSelection() {
       moveRight();
     }
   } else */
-  if (13 == total) {
+  if (13 == total)
+  {
     removeCardFromBoard(cardOne);
     removeCardFromBoard(cardTwo);
-    if (flipIsSelected) {
+    if (flipIsSelected)
+    {
       deck[discardPtr] = 0;
       moveBackDiscard();
     }
     resetSelections();
     determineSelectability();
-    if (isOnBoard) {
-      moveRightRedux();
+    scanForEligibility();
+    if (isOnBoard)
+    {
+      moveLeftUsingEligibility();
     }
   }
 }
 
-void checkInput() {
-  if (player1_new_buttons & INPUT_MASK_RIGHT) {
-    moveRightRedux();
-  } else if (player1_new_buttons & INPUT_MASK_LEFT) {
-    moveLeftRedux();
-  } else if (player1_new_buttons & INPUT_MASK_DOWN) {
+void checkInput()
+{
+  if (player1_new_buttons & INPUT_MASK_RIGHT)
+  {
+    moveRightUsingEligibility();
+  }
+  else if (player1_new_buttons & INPUT_MASK_LEFT)
+  {
+    // moveLeftRedux();
+    moveLeftUsingEligibility();
+  }
+  else if (player1_new_buttons & INPUT_MASK_DOWN)
+  {
     isOnBoard = false;
     isOnDiscard = true;
-  } else if (player1_new_buttons & INPUT_MASK_UP) {
+  }
+  else if (player1_new_buttons & INPUT_MASK_UP)
+  {
     isOnBoard = true;
     isOnDiscard = false;
-  } else if (player1_new_buttons & INPUT_MASK_B) {
+  }
+  else if (player1_new_buttons & INPUT_MASK_B)
+  {
     resetSelections();
-  } else if (player1_new_buttons & INPUT_MASK_A) {
-    if (numSelections < 2) {
-      if (isOnDiscard) {
+  }
+  else if (player1_new_buttons & INPUT_MASK_A)
+  {
+    if (numSelections < 2)
+    {
+      if (isOnDiscard)
+      {
         flipIsSelected = true;
         numSelections++;
-      } else {
+      }
+      else
+      {
 
         select[cursorRow][cursorCard] = 1;
         numSelections++;
       }
-    } else {
+    }
+    else
+    {
       resetSelections();
 
-      if (isOnDiscard) {
+      if (isOnDiscard)
+      {
         flipIsSelected = true;
         numSelections++;
-      } else {
+      }
+      else
+      {
         select[cursorRow][cursorCard] = 1;
         numSelections++;
       }
     }
     checkSelection();
-  } else if (player1_new_buttons & INPUT_MASK_C) {
+  }
+  else if (player1_new_buttons & INPUT_MASK_C)
+  {
     advanceNextDiscard();
   }
 }
 
-void renderPyramidBoard() {
+void renderPyramidBoard()
+{
   int rows, cols, cardx, cardy, counter;
 
   counter = 1;
@@ -352,27 +529,34 @@ void renderPyramidBoard() {
 
   queue_draw_sprite(0, 0, 127, 127, 0, 0, background);
 
-  for (rows = 0; rows < 7; rows++) {
+  for (rows = 0; rows < 7; rows++)
+  {
     cardy = divy[rows];
 
-    if (rows != 0) {
+    if (rows != 0)
+    {
       cardy -= rows * 4;
       cardx = 64 - ((16 * rows) / 2);
     }
 
-    for (cols = 0; cols < counter; cols++) {
-      if (cursorRow == rows && cursorCard == cols && isOnBoard) {
-        queue_draw_box(cardx - 8, cardy - 8, 16, 16, rnd_range(0, 255));
-      }
+    for (cols = 0; cols < counter; cols++)
+    {
 
-      if (board[rows][cols] != 0) {
+      if (board[rows][cols] != 0)
+      {
         // If the cursor is currently over this
-
+        if (cursorRow == rows && cursorCard == cols && isOnBoard)
+        {
+          queue_draw_box(cardx - 8, cardy - 8, 16, 16, rnd_range(0, 255));
+        }
         // If the card we are is has a selection flag on it
-        if (select[rows][cols] & 1) {
+        if (select[rows][cols] & 1)
+        {
           queue_draw_sprite_frame(testSlot, cardx, cardy + 8, board[rows][cols],
                                   false);
-        } else {
+        }
+        else
+        {
           queue_draw_sprite_frame(testSlot, cardx, cardy, board[rows][cols],
                                   false);
         }
@@ -382,21 +566,28 @@ void renderPyramidBoard() {
     counter++;
   }
 
-  if (!isOnBoard && isOnDiscard) {
+  if (!isOnBoard && isOnDiscard)
+  {
     queue_draw_box(FLIPPED_PILE_X - 8, FLIPPED_PILE_Y - 8, 16, 16,
                    rnd_range(0, 255));
   }
 
   queue_draw_sprite_frame(testSlot, DISCARD_PILE_X, DISCARD_PILE_Y, 0, false);
-  if (discardPtr > DISCARD_BEGINS) {
-    if (flipIsSelected) {
+  if (discardPtr > DISCARD_BEGINS)
+  {
+    if (flipIsSelected)
+    {
       queue_draw_sprite_frame(testSlot, FLIPPED_PILE_X + 8, FLIPPED_PILE_Y,
                               deck[discardPtr], false);
-    } else {
+    }
+    else
+    {
       queue_draw_sprite_frame(testSlot, FLIPPED_PILE_X, FLIPPED_PILE_Y,
                               deck[discardPtr], false);
     }
-  } else {
+  }
+  else
+  {
     queue_draw_sprite_frame(testSlot, FLIPPED_PILE_X, FLIPPED_PILE_Y, 0, false);
   }
 
